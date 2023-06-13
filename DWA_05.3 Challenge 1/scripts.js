2762d1daa054a6590824666ff9f23b96bfa26bd2
Woliver4 <126001715+Woliver4@users.noreply.github.com>
2023-06-13 13:15:51 +0200

    Add files via upload

---
+++ DWA_05.3 Challenge 1/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  if (dividend === "" || divider === "" ){
+        result.innerText = "Division not performed. Both values are required in inputs. Try again."
+    } else if (dividend < 1 || divider < 1 ){
+            result.innerText = "Division not performed. Invalid number provided. Try again."
+        } else if (isNaN(dividend) || isNaN(divider) ){
+            result.innerText = "Something critical went wrong. Please reload the page."
+        } else {
+        result.innerText = Math.floor(dividend / divider);
+    }
+});
